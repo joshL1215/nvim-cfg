d8435face62ddaee133775860218a1e650d279d1
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2026-01-16 15:45:23 -0800

    make Clw uppercase

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-vim.api.nvim_create_user_command('clw', function()
+vim.api.nvim_create_user_command('Clw', function()
   vim.cmd 'vsplit'
 
   local width = math.floor(vim.o.columns * 0.3)
