6e19eff7ce9dbab6e5108cfec2e904f18bf313e9
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2026-01-16 15:48:35 -0800

    add open terminal split command

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+-- New Claude CLI window on right
 vim.api.nvim_create_user_command('Clw', function()
   vim.cmd 'vsplit'
 
@@ -9,5 +10,16 @@ vim.api.nvim_create_user_command('Clw', function()
   vim.cmd 'terminal claude'
   vim.cmd 'startinsert'
 end, { desc = 'Open Claude in right-side terminal split' })
+
+-- New terminal window on right
+vim.api.nvim_create_user_command('Term', function()
+  vim.cmd 'vsplit'
+
+  local width = math.floor(vim.o.columns * 0.3)
+  vim.cmd('vertical resize ' .. width)
+
+  vim.cmd 'terminal'
+  vim.cmd 'startinsert'
+end, { desc = 'Open terminal split on right' })
 
 return M
