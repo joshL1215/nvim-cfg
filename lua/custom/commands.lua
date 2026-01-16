f7c6448addc34bf55c12d74cd5e03a93339e41b2
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2026-01-16 15:44:46 -0800

    make clw command lowercase and make insert mode on clw automatically

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,12 +1,13 @@
 local M = {}
 
-vim.api.nvim_create_user_command('ClaudeWindow', function()
+vim.api.nvim_create_user_command('clw', function()
   vim.cmd 'vsplit'
 
   local width = math.floor(vim.o.columns * 0.3)
   vim.cmd('vertical resize ' .. width)
 
   vim.cmd 'terminal claude'
+  vim.cmd 'startinsert'
 end, { desc = 'Open Claude in right-side terminal split' })
 
 return M
